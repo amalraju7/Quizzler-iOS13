48cdd79d7b8e60c1907544cc70318c4bf5fec6db
amalraju7 <amalraju443@gmail.com>
2022-07-18 12:25:28 +0530

    Questions added

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -7,11 +7,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
     
     let quiz = [
-        ["Four + Two is equal to six","True"],
-        ["Five - Three is greater than One","False"],
-    ["Three + Eight is less than Ten","True"],
+        Question(q: "A slug's blood is green.", a: "True"),
+               Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+               Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+               Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+               Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+               Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+               Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+               Question(q: "Google was originally called 'Backrub'.", a: "True"),
+               Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+               Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+               Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+               Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True"),
     ]
     var questionNumber = 0
     override func viewDidLoad() {
@@ -21,15 +31,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        var actualAnswer = quiz[questionNumber][1]
+        var actualAnswer = quiz[questionNumber].answer
         var userAnswer = sender.currentTitle
         
         if userAnswer == actualAnswer
         {
-            print("Right")
+            sender.backgroundColor = UIColor.green
         }
         else{
-            print("Wrong")
+            sender.backgroundColor = UIColor.red
         }
         
         if(questionNumber+1 < quiz.count){
@@ -39,12 +49,16 @@ class ViewController: UIViewController {
             questionNumber = 0
         }
         
-       updateUI()
+        Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
         
     }
     
-    func updateUI(){
-        questionLabel.text = quiz[questionNumber][0]
+    @objc func updateUI(){
+        questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = Float(questionNumber+1) / Float(quiz.count)
+        
     }
     
 }
