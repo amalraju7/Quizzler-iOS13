9c1bf3aa3666197f2a60f82c313f1b21e3db59f5
amalraju7 <amalraju443@gmail.com>
2022-07-18 11:05:44 +0530

    quiz implemented with 2D ARRAY

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -1,20 +1,51 @@
-//
-//  ViewController.swift
-//  Quizzler-iOS13
-//
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        ["Four + Two is equal to six","True"],
+        ["Five - Three is greater than One","False"],
+    ["Three + Eight is less than Ten","True"],
+    ]
+    var questionNumber = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        var actualAnswer = quiz[questionNumber][1]
+        var userAnswer = sender.currentTitle
+        
+        if userAnswer == actualAnswer
+        {
+            print("Right")
+        }
+        else{
+            print("Wrong")
+        }
+        
+        if(questionNumber+1 < quiz.count){
+            questionNumber += 1
+        }
+        else{
+            questionNumber = 0
+        }
+        
+       updateUI()
+        
+    }
+    
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber][0]
+    }
+    
 }
 
